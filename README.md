1ad44f7c065eefa75cf65053bf6935fddf55016a
Henry Hernandez <hg23giovann@hotmail.com>
2022-05-11 01:23:20 -0500

    Fuguras geometricas JS

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # platzi-cursopractico-javascript
+
+## Taller #1: figuras geométricas
+
+ - Primer paso: definir as formulas
+ - Segundo: implementar las formulas en JS
+ - Tercer Paso: crear funciones
+ - Cuarto Paso: Integrar Java Script con Html
---
+++ figuras.js
@@ -0,0 +1,54 @@
+// Código el cuadrdo
+
+console.group("Cuadrados");
+
+
+const ladoCuadrado = 5;
+console.log("los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetrocuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrdo es: " + perimetrocuadrado + "cm");
+
+const areacuadro = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadro es: " + areacuadro + " cm^2");
+
+console.groupEnd();
+
+//Códifo del triangulo
+
+console.group("Triangulo");
+
+const ladotriangulo1 = 5;
+const ladotriangulo2 = 5;
+const basetriangulo = 15;
+
+console.log(
+    "los lados del triángulo miden: " 
+    + ladotriangulo1 
+    + "cm," 
+    + ladotriangulo2 
+    + "cm," 
+    + basetriangulo 
+    + "cm"
+    );
+
+const alturaTriangulo =5.5;
+console.log("La altura del tiangulo miden: " + alturaTriangulo + "cm");
+
+const perimetroTriangulo =5.5;
+console.log("La perimetro del tiangulo es: " + ladotriangulo1 + "cm" + ladotriangulo2 + "cm" + basetriangulo + "cm");
+
+const areatriangulo = (basetriangulo * alturaTriangulo) / 2;
+console.log("El área del triangulo es: " + areatriangulo + " cm^2");
+
+console.groupEnd();    
+
+
+
+console.group(Circulos);
+
+//Código Circulos
+
+//const radiocirculo = 
+
+console.groupEnd();
